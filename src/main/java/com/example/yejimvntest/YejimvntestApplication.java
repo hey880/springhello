e8feba1039a04180eb3d0e694e41e2397589e437
yeji <hey880@naver.com>
2026-07-06 13:48:40 +0900

    modify: change gradle to maven

---
+++ src/main/java/com/example/yejimvntest/YejimvntestApplication.java
@@ -0,0 +1,13 @@
+package com.example.yejimvntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YejimvntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YejimvntestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yejimvntest/YejimvntestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yejimvntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YejimvntestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
