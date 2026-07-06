6369ca926262e9eed1212d8a5ad147200aec1e96
yeji <hey880@naver.com>
2026-07-06 12:21:48 +0900

    feature: hello

---
+++ src/main/java/com/example/yejitest/YejitestApplication.java
@@ -0,0 +1,13 @@
+package com.example.yejitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YejitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YejitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yejitest/YejitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yejitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YejitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
